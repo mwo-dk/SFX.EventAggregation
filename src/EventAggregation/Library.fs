050998ee7965ada124d1d7e9090830436e0f324e
Michael Wiinberg Olesen <michael.w.olesen@outlook.com>
2020-01-04 15:00:36 +0100

    Added Xml comments

--- src/EventAggregation/Library.fs
+++ src/EventAggregation/Library.fs
@@ -6,44 +6,70 @@ open SFX.ROP
 open SFX.EventAggregation.Model
 open SFX.EventAggregation.Infrastructure
 
+/// Creates an EventAggregator<>
 let createEventAggregator() = new EventAggregator<'a>()
 
+/// Alias for: 'a -> unit
 type SyncHandler<'a> = 'a -> unit
+/// Alias for : 'a -> Async<unit>
 type AsyncHandler<'a> = 'a -> Async<unit>
+/// Represents a subscriber
 type Subscriber<'a> =
+/// The subscriber is an IHandle<>
 | Sync of IHandle<'a>
+/// The subscriber is an IHandle<> and requires dispatching via a SynchronizationContext
 | SyncWithContext of IHandle<'a>*SynchronizationContext
+/// The subscriber is an IHandleAsync<>
 | Async of IHandleAsync<'a>
-| AsyncWithContext of IHandle<'a>*SynchronizationContext
+/// The subscriber is an IHandleAsync<> and requires dispatching via a SynchronizationContext
+| AsyncWithContext of IHandleAsync<'a>*SynchronizationContext
+/// The subscriber is an SyncHandler<>
 | SyncHandler of SyncHandler<'a>
+/// The subscriber is an SyncHandler<> and requires dispatching via a SynchronizationContext
 | SyncHandlerWithContext of SyncHandler<'a>*SynchronizationContext
+/// The subscriber is an AsyncHandler<>
 | AsyncHandler of AsyncHandler<'a>
+/// The subscriber is an AsyncHandler<> and requires dispatching via a SynchronizationContext
 | AsyncHandlerWithContext of AsyncHandler<'a>*SynchronizationContext
+/// Creates a Subscriber from a SyncHandler<>
 let sync = SyncHandler
+/// Creates a Subscriber from a SyncHandler and a SynchronizationContext
 let syncWithContext x y = SyncHandlerWithContext(x,y)
+/// Creates a Subscriber from an AsyncHandler<>
 let async = AsyncHandler
+/// Creates a Subscriber from an AsyncHandler and a SynchronizationContext
 let asyncWithContext x y = AsyncHandlerWithContext(x,y)
 
+/// Represents a subscription id - alias to int64
 type Subscription = int64
 
+/// Represents errors, that can occur when working with EventAggregators
 type EventAggregatorError =
+/// The lock was not acquired when subscribing or unubscribing
 | LockNotAcquired
+/// Occurs when trying to unsubscribe and the actual inner subscription was not found
 | EventAggregatorNotFound
+/// The denoted subscription was not found
 | SubscriptionNotFound of Subscription
 
+/// Represents a synchronous handle object from a SyncHandler<>
 type Handle<'a>(handler: SyncHandler<'a>) =
     interface IHandle<'a> with
         member _.Handle message = message |> handler
+/// Creates a Handle<>
 let createHandle x = Handle(x)
+/// Represents an asynchronous handle object from a SyncHandler<>
 type AsyncHandle<'a>(handler: AsyncHandler<'a>) =
     interface IHandleAsync<'a> with
         member _.HandleAsync message = 
             Tasks.Task.Run(fun () -> 
                 message |> handler |> Async.RunSynchronously)
+/// Creates an AsyncHandle<>
 let createAsyncHandle x = AsyncHandle(x)
 
 // This tiny infrastructure is in order to hold on to else temporary objects created to be
 // registered inside event aggregators - in the functional cases.
+
 let mutable private lock = SpinLock()
 let mutable private hiddenEntities : (WeakReference * Map<Subscription, obj>) list = List.empty
 let private addHiddenEntity id x evtAgg =
@@ -87,6 +113,8 @@ let private removeHiddenEntity id evtAgg =
         if lockTaken then
             lock.Exit(false)
 
+/// Subscribes the handler to the evtAgg (IEventAggregator<>). The flag
+/// serialize denotes whether to serialize messages published
 let subscribe serialize handler (evtAgg: IEventAggregator<'a>) =
     let doSyncHandler x s =
         let x = Handle(x)
@@ -109,7 +137,8 @@ let subscribe serialize handler (evtAgg: IEventAggregator<'a>) =
     | SyncHandlerWithContext (x, s) -> doSyncHandler x s
     | AsyncHandler x -> doAsyncHandler x null
     | AsyncHandlerWithContext (x, s) -> doAsyncHandler x s
-        
+      
+/// Removes the subscription denoted by id from evtAgg (IEventAggregator<>)
 let unsubscribe id (evtAgg: IEventAggregator<'a>) =
     if evtAgg.Unsubscribe id then
         match evtAgg |> removeHiddenEntity id with
@@ -120,19 +149,28 @@ let unsubscribe id (evtAgg: IEventAggregator<'a>) =
             | _ -> () |> succeed
     else id |> SubscriptionNotFound |> fail
 
+/// Publishes the message via evtAgg (IEventAggregator<>)
 let publish message (evtAgg: IEventAggregator<'a>) = evtAgg.Publish message
 
+/// Creates an EventAggregatorRepository
 let createEventAggregatorRepository() =
     EventAggregatorRepository()
+/// Initializes an IEventAggregatorRepository
 let initializeEventAggregatorRepository (repo: IEventAggregatorRepository) =
     repo.Initialize()
+/// Checkes whether an IEventAggregatorRepository is initialized
 let isEventAggregatorRepositoryInitialized (repo: IEventAggregatorRepository) =
     repo.IsInitialized()
 
+/// Represents a "kind" of event aggregator. Anonymous means "the one" denoted
+/// by type only. Named means a dedicated "bus" with a given type
 type EventAggregatorInstance =
 | Anonymous
 | Named of string
+/// Creates the name
 let named = Named
+/// Gets an IEventAggregator from repo (IEventAggregatorRepository). If instance
+/// is Anonymous the default instance is returned, else the one denoted by the name
 let getEventAggregator instance (repo: IEventAggregatorRepository) = 
     match instance with
     | Anonymous -> repo.GetEventAggregator()
